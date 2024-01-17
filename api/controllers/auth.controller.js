eacaa9e236c9c6241a1ec01bcc1b6ee085115d7a
sneha sundaran mp <snehapallippuram@gmail.com>
2024-01-17 11:32:14 +0530

    create user model

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ const hashedPassword =bcryptjs.hashSync(password, 10);
 const newUser =new User({username,email,password:hashedPassword});
 try{
     await newUser.save();
-    res.status(201).json('User created sucessfully!');
+    res.status(201).json('User created sucessfully!!!');
 } catch (error){
     next(error)
 }
